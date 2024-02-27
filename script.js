cd61c725124139fbf116713167e69065e37311a1
Supravisor <153783117+Supravisor@users.noreply.github.com>
2024-02-27 15:37:18 +1300

    script.js (#4)

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
 import ReactDOM from "https://esm.sh/react-dom";
 
-ReactDOM.render(JSX, document.getElementById('app'));
+
+const randomColor = () => {
+  const hexadecimalNumbers = '0123456789ABCDEF';
+  let colour = '#';
+  for (let i = 0; i < 6; i++) {
+    colour += hexadecimalNumbers[Math.floor(Math.random() * 16)];
+  }
+  return colour;
+};
+
+let colour = randomColor();
+
+let reverseColour = () => {
+  let tempColour = colour.slice();
+  let reversedColour = tempColour[0];
+  for (let i = 6; i > 0; i--) {
+    reversedColour += tempColour[i];
+  }
+  return reversedColour;
+};
+
+let backColour = reverseColour();
+
+const JSX = /*#__PURE__*/
+React.createElement("div", { style: { background: colour } }, /*#__PURE__*/
+React.createElement("h1", { style: { color: backColour } }, "Hello World"));
+
+
+
+ReactDOM.render(JSX, document.getElementById('test'));
