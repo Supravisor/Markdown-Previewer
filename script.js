4c2404f54c75415224d2e1f3a17067e591612532
Supravisor <153783117+Supravisor@users.noreply.github.com>
2024-02-28 15:01:34 +1300

    script.js
    script.js

--- script.js
+++ script.js
@@ -1,32 +1,66 @@
 import ReactDOM from "https://esm.sh/react-dom";
 
-
-const randomColor = () => {
-  const hexadecimalNumbers = '0123456789ABCDEF';
-  let colour = '#';
-  for (let i = 0; i < 6; i++) {
-    colour += hexadecimalNumbers[Math.floor(Math.random() * 16)];
+class Input extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ''
+    };
+    // Change code below this line
+    this.handleChange = this.handleChange.bind(this)
+    // Change code above this line
   }
-  return colour;
+  // Change code below this line
+    handleChange (event) {
+    this.setState({
+      input: event.target.value
+    });
+  }
+
+  // Change code above this line
+  render() {
+    return (
+      <div>
+      <textarea value = {this.state.input} onChange = {this.handleChange} ></textarea>
+  <Output output={this.state.input} />
+      </div>
+    );
+  }
 };
+
+
+const Output = (props) => <div>
+    <h4>Controlled Input:</h4>
+        <p>{props.output}</p>     
+      </div>;
+
+
+  const randomColor = () => {
+    const hexadecimalNumbers = '0123456789ABCDEF';
+    let colour = '#';
+    for (let i = 0; i < 6; i++) {
+      colour += hexadecimalNumbers[Math.floor(Math.random() * 16)];
+    }
+    return colour;
+  };
 
 let colour = randomColor();
 
 let reverseColour = () => {
   let tempColour = colour.slice();
   let reversedColour = tempColour[0];
-  for (let i = 6; i > 0; i--) {
+  for (let i = 6; i > 0 ; i--) {
     reversedColour += tempColour[i];
-  }
-  return reversedColour;
-};
+  } 
+    return reversedColour;
+}
 
 let backColour = reverseColour();
 
-const JSX = /*#__PURE__*/
-React.createElement("div", { style: { background: colour } }, /*#__PURE__*/
-React.createElement("h1", { style: { color: backColour } }, "Hello World"));
+const JSX = (
+  <div style={{background: colour }}>
+    <h1 style={{color: backColour }}>Hello World</h1>
+  </div>
+);
 
-
-
-ReactDOM.render(JSX, document.getElementById('test'));
+ReactDOM.render(<Input />, document.getElementById('editor'));
