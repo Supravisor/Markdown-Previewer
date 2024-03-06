f6a61ee7403cb5cfd9d93a1960e04efff753722a
Supravisor <153783117+Supravisor@users.noreply.github.com>
2024-03-07 12:46:43 +1300

    script.js
    script.js

--- script.js
+++ script.js
@@ -8,39 +8,33 @@ class Input extends React.Component {
     this.state = {
       markdown: "" };
 
-    // Change code below this line
-
-    // Change code above this line
   }
 
-  // Change code below this line
   updateMarkdown(markdown) {
     this.setState({ markdown });
   }
 
-  // Change code above this line
   render() {
     return /*#__PURE__*/(
-      React.createElement("div", null, /*#__PURE__*/
-      React.createElement("textarea", { 
-        id: "editor",
+      React.createElement("div", { class: "container" }, /*#__PURE__*/
+      React.createElement("textarea", { id: "editor",
         value: this.state.markdown,
         onChange: e => {this.updateMarkdown(e.target.value);
         } },
 
       console.log(this.state.markdown)), /*#__PURE__*/
 
-      React.createElement("div", { dangerouslySetInnerHTML: { __html: this.state.markdown } }), /*#__PURE__*/
+      React.createElement("div", { id: "preview", class: "container", dangerouslySetInnerHTML: { __html: this.state.markdown } }), /*#__PURE__*/
 
       React.createElement(Output, null)));
-
 
   }}
 
 
 const Output = (props) => /*#__PURE__*/
-React.createElement("div", { id: "preview" }, /*#__PURE__*/
+React.createElement("div", null, /*#__PURE__*/
 
 React.createElement("div", null, props.output));
+
 
 ReactDOM.render( /*#__PURE__*/React.createElement(Input, null), document.getElementById("markdown"));
