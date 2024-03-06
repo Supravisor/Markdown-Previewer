48656ceec6e6c32fb5633913c7273c6008bb2b81
Supravisor <153783117+Supravisor@users.noreply.github.com>
2024-03-06 18:43:03 +1300

    script.js
    script.js

--- script.js
+++ script.js
@@ -43,36 +43,4 @@ React.createElement("div", { id: "preview" }, /*#__PURE__*/
 
 React.createElement("div", null, props.output));
 
-
-
-const randomColor = () => {
-  const hexadecimalNumbers = "0123456789ABCDEF";
-  let colour = "#";
-  for (let i = 0; i < 6; i++) {
-    colour += hexadecimalNumbers[Math.floor(Math.random() * 16)];
-  }
-  return colour;
-};
-
-let colour = randomColor();
-
-let reverseColour = () => {
-  let tempColour = colour.slice();
-  let reversedColour = tempColour[0];
-  for (let i = 6; i > 0; i--) {
-    reversedColour += tempColour[i];
-  }
-  return reversedColour;
-};
-
-let backColour = reverseColour();
-
-const JSX = /*#__PURE__*/
-React.createElement("div", { style: { background: colour } }, /*#__PURE__*/
-React.createElement("h1", { style: { color: backColour } }, "Hello World"));
-
-
-
 ReactDOM.render( /*#__PURE__*/React.createElement(Input, null), document.getElementById("markdown"));
-
-ReactDOM.render(JSX, document.getElementById("test"));
